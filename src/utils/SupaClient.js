6595ac616ce06bf5e1f0326589254823e8e58222
raihan mahdy <mahdyraihan047@gmail.com>
2024-11-06 13:40:06 +0700

    p

--- src/utils/SupaClient.js
+++ src/utils/SupaClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase_url = import.meta.env.VITE_SUPABASE_URL
-const supabase_key = import.meta.env.VITE_SUPABASE_KEY
+const supabase_url = "https://jhusxvxjjuvpexotajto.supabase.co/"
+const supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpodXN4dnhqanV2cGV4b3RhanRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4MTExMzQsImV4cCI6MjA0MDM4NzEzNH0.qijm29VWB8-ncZXfkvZo3uLaSGHUfhrLqotZtPZGrpA"
 
 export const supabase = createClient(supabase_url, supabase_key);
